53c2c3abbe6b7f03a1e31506eb4564e196186b90
0w4n <jorgelevia@gmail.com>
2022-04-27 20:28:10 +0200

    inicio

---
+++ Dinosaurio.cs
@@ -0,0 +1,10 @@
+class Dinosaurio{
+    float x = width/2, y= height/2;
+    Dinosaurio(){}
+}
+
+
+void display(){
+    fill(255,0,0);
+    rect(x,y,20,20);
+}
---
+++ main.cs
@@ -0,0 +1,10 @@
+Dinosaurio d1 = new Dinosaurio();
+
+void Setup() {
+    size(1080,720);
+}
+
+void draw() {
+    background(0);
+    d1.display();
+}
